b66db5f76900c07fd296219d6861e31dbea24f97
Dean Ellis <dellis1972@googlemail.com>
2024-06-12 23:21:52 +0100

    Fix Windows build

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -12,7 +12,7 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
         var buildWorkingDir = "basis_universal/";
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "CMakeLists.txt" });
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "--build . --config release" });
-        var files = Directory.GetFiles(System.IO.Path.Combine (buildWorkingDir, "bin"), "basisu.exe", SearchOption.TopDirectoryOnly);
+        var files = Directory.GetFiles(System.IO.Path.Combine (buildWorkingDir, "bin", "Release"), "basisu.exe", SearchOption.TopDirectoryOnly);
         context.CopyFile(files[0], $"{context.ArtifactsDir}/basisu.exe");
     }
 }
