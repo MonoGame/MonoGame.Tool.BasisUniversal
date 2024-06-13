5b1fabf5a1963b6227b691edf8939453684231b1
Dean Ellis <dellis1972@googlemail.com>
2024-06-13 09:56:00 +0100

    Some changes

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -10,7 +10,8 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
     public override void Run(BuildContext context)
     {
         var buildWorkingDir = "basis_universal/";
-        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DSAN=ON CMakeLists.txt" });
+        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DSAN=ON CMakeLists.txt" });
+        context.ReplaceTextInFiles("basis_universal/basisu.vcxproj", "MultiThreadedDLL", "MultiThreaded");
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "--build . --config release" });
         var files = Directory.GetFiles(System.IO.Path.Combine (buildWorkingDir, "bin", "Release"), "basisu.exe", SearchOption.TopDirectoryOnly);
         context.CopyFile(files[0], $"{context.ArtifactsDir}/basisu.exe");
