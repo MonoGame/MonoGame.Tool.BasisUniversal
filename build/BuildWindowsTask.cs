1693feb021ef39550209283da73fb7a2a4363188
harry.cpp <harry.cpp@outlook.com>
2025-05-23 23:01:37 +0200

    Still blind fixing Windows

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -15,7 +15,8 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
         context.ReplaceTextInFiles("basis_universal/CMakeLists.txt", "project(basisu)", "project(basisu C CXX)\nset(CMAKE_CXX_STANDARD 17)");
 
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DSAN=ON CMakeLists.txt" });
-        context.ReplaceTextInFiles("basis_universal/basisu.vcxproj", "MultiThreadedDLL", "MultiThreaded");
+        var runtimeLibLine = "<PreprocessorDefinitions>NDEBUG;_HAS_EXCEPTIONS=0;%(PreprocessorDefinitions);BASISU_SUPPORT_SSE=1;BASISU_SUPPORT_OPENCL=1;</PreprocessorDefinitions>";
+        context.ReplaceTextInFiles("basis_universal/basisu.vcxproj", runtimeLibLine, $"{runtimeLibLine}\n<RuntimeLibrary>MultiThreaded</RuntimeLibrary>");
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "--build . --config release" });
         var files = Directory.GetFiles(System.IO.Path.Combine(buildWorkingDir, "bin", "Release"), "basisu.exe", SearchOption.TopDirectoryOnly);
         context.CopyFile(files[0], $"{context.ArtifactsDir}/basisu.exe");
