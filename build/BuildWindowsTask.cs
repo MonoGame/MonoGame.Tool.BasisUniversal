c0966271b865b13d0d8913583b989faf4155db0f
Tom Spilman <tom@sickheadgames.com>
2025-05-24 06:36:36 -0500

    Fixed build to static link the runtime.

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -16,6 +16,8 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
 
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DSAN=ON -DSTATIC=TRUE CMakeLists.txt" });
         context.ReplaceTextInFiles("basis_universal/basisu.vcxproj", "MultiThreadedDLL", "MultiThreaded");
+        context.ReplaceTextInFiles("basis_universal/basisu_encoder.vcxproj", "MultiThreadedDLL", "MultiThreaded");
+        context.ReplaceTextInFiles("basis_universal/examples.vcxproj", "MultiThreadedDLL", "MultiThreaded");
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "--build . --config release" });
         var files = Directory.GetFiles(System.IO.Path.Combine(buildWorkingDir, "bin"), "basisu.exe", SearchOption.TopDirectoryOnly);
         context.CopyFile(files[0], $"{context.ArtifactsDir}/basisu.exe");
