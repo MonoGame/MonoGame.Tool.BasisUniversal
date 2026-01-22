4441e64cfa4b5d33dbcf95d1e5434646a512d62e
Dean Ellis <dellis1972@googlemail.com>
2026-01-22 19:38:08 +0000

    Add OSX deployment target to CMake arguments

--- build/BuildMacOSTask.cs
+++ build/BuildMacOSTask.cs
@@ -10,7 +10,7 @@ public sealed class BuildMacOSTask : FrostingTask<BuildContext>
     public override void Run(BuildContext context)
     {
         var buildWorkingDir = "basis_universal/";
-        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DCMAKE_OSX_ARCHITECTURES=\"x86_64;arm64\" CMakeLists.txt" });
+        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -DCMAKE_OSX_ARCHITECTURES=\"x86_64;arm64\" CMakeLists.txt" });
         context.StartProcess("make", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "" });
         var files = Directory.GetFiles(System.IO.Path.Combine(buildWorkingDir, "bin"), "basisu", SearchOption.TopDirectoryOnly);
         context.CopyFile(files[0], $"{context.ArtifactsDir}/basisu");
