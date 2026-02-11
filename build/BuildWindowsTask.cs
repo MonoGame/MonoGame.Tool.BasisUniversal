2371a8a84adcf3c3348413e6694b102d08c2bd81
Dean Ellis <dellis1972@googlemail.com>
2026-02-11 13:20:45 +0000

    Try to build for win-arm64

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -9,17 +9,21 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
 
     public override void Run(BuildContext context)
     {
-        var buildWorkingDir = "basis_universal/";
+        BuildForArchitecture(context, "x64", "windows-x64");
+        BuildForArchitecture(context, "ARM64", "windows-arm64");
+    }
 
-        // needed until https://github.com/BinomialLLC/basis_universal/pull/391 gets merged
+    private void BuildForArchitecture(BuildContext context, string arch, string rid)
+    {
+        var buildWorkingDir = $"basis_universal/{rid}";
         context.ReplaceTextInFiles("basis_universal/CMakeLists.txt", "project(basisu)", "project(basisu C CXX)\nset(CMAKE_CXX_STANDARD 17)");
 
-        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DSAN=ON -DSTATIC=TRUE CMakeLists.txt" });
+        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = $"-DSAN=ON -A {arch} -DSTATIC=TRUE CMakeLists.txt" });
         context.ReplaceTextInFiles("basis_universal/basisu.vcxproj", "MultiThreadedDLL", "MultiThreaded");
         context.ReplaceTextInFiles("basis_universal/basisu_encoder.vcxproj", "MultiThreadedDLL", "MultiThreaded");
         context.ReplaceTextInFiles("basis_universal/examples.vcxproj", "MultiThreadedDLL", "MultiThreaded");
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "--build . --config release" });
         var files = Directory.GetFiles(System.IO.Path.Combine(buildWorkingDir, "bin"), "basisu.exe", SearchOption.TopDirectoryOnly);
-        context.CopyFile(files[0], $"{context.ArtifactsDir}/basisu.exe");
+        context.CopyFile(files[0], $"{context.ArtifactsDir}/{rid}/basisu.exe");
     }
 }
