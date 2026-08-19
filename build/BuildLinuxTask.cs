46bd1cafa217e2069d4f569d949d6e2d330f662f
Dean Ellis <dellis1972@googlemail.com>
2026-08-19 16:46:53 +0100

    String binaries

--- build/BuildLinuxTask.cs
+++ build/BuildLinuxTask.cs
@@ -10,9 +10,15 @@ public sealed class BuildLinuxTask : FrostingTask<BuildContext>
     public override void Run(BuildContext context)
     {
         var buildWorkingDir = "basis_universal/";
-        context.StartProcessWithDocker("cmake", workingDirectory: buildWorkingDir, args: "CMakeLists.txt");
-        context.StartProcessWithDocker("make", workingDirectory: buildWorkingDir, args: "");
+        context.StartProcessWithDocker("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "CMakeLists.txt" });
+        context.StartProcessWithDocker("make", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "" });
         var files = Directory.GetFiles(System.IO.Path.Combine(buildWorkingDir, "bin"), "basisu", SearchOption.TopDirectoryOnly);
-        context.CopyFile(files[0], $"{context.ArtifactsDir}/basisu");
+        var artifact = $"{context.ArtifactsDir}/basisu";
+        context.CopyFile(files[0], artifact);
+        
+        var stripArguments = new ProcessArgumentBuilder();
+        stripArguments.Append("--strip-unneeded");
+        stripArguments.AppendQuoted(artifact);
+        context.StartProcessWithDocker("strip", new ProcessSettings { WorkingDirectory = "", Arguments = stripArguments });
     }
 }
